635e941e5164808a5074c69183e0a615f9cd64b2
信风 <2426412613@qq.com>
2021-09-15 17:28:31 +0800

    fix: 批准直播请求的同时为其分配一个房间

--- src/main/java/com/xinf/entity/Room.java
+++ src/main/java/com/xinf/entity/Room.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,6 +21,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Room implements Serializable {
     private static final long serialVersionUID = -40995804493033969L;
 
--- src/main/java/com/xinf/service/impl/ApplyAnchorRecordServiceImpl.java
+++ src/main/java/com/xinf/service/impl/ApplyAnchorRecordServiceImpl.java
@@ -5,8 +5,10 @@ import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xinf.dao.ApplyAnchorRecordDao;
 import com.xinf.entity.ApplyAnchorRecord;
+import com.xinf.entity.Room;
 import com.xinf.entity.User;
 import com.xinf.service.ApplyAnchorRecordService;
+import com.xinf.service.RoomService;
 import com.xinf.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,19 +22,28 @@ import java.util.Map;
  * @author makejava
  * @since 2021-09-11 11:17:08
  */
+
 @Service("applyAnchorRecordService")
 public class ApplyAnchorRecordServiceImpl extends ServiceImpl<ApplyAnchorRecordDao, ApplyAnchorRecord> implements ApplyAnchorRecordService {
 
     @Resource
     UserService userService;
+    @Resource
+    RoomService roomService;
 
     @Override
     @Transactional
     public boolean updateById(ApplyAnchorRecord entity) {
         if (entity.getApplyResult() == 1) {
-            Map<String, Object> map = userService.getMap(new QueryWrapper<User>().select("role_id").eq("user_id", entity.getUserId()));
+            Map<String, Object> map = userService.getMap(new QueryWrapper<User>().select("role_id", "room_id").eq("user_id", entity.getUserId()));
             if ((Integer)map.get("role_id") < 11) {
                 userService.update(new UpdateWrapper<User>().eq("user_id", entity.getUserId()).set("role_id", 11));
+            }
+            if ((long)map.get("room_id") == 0) {
+                // 添加一个房间
+                roomService.save(Room.builder().userId(entity.getUserId()).build());
+                Map<String, Object> map1 = roomService.getMap(new QueryWrapper<Room>().select("room_id").eq("user_id", entity.getUserId()));
+                userService.updateById(User.builder().userId(entity.getUserId()).roomId((long)map1.get("room_id")).build());
             }
         }
         return super.updateById(entity);
