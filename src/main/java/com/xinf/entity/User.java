f78970f3dab4a1c7eae5335582472cd96c09a1c1
信风 <2426412613@qq.com>
2021-09-15 17:28:31 +0800

    fix: 批准直播请求的同时为其分配一个房间

--- src/main/java/com/xinf/entity/User.java
+++ src/main/java/com/xinf/entity/User.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,6 +20,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User implements Serializable {
     private static final long serialVersionUID = 992236825819524628L;
 
