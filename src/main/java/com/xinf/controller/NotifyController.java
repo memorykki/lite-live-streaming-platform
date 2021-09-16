0c8772372a62b67ed4bd78fece47e7e2c6dd0899
“memorykk” <memorykk@qq.com>
2021-09-16 10:56:20 +0800

    测试回调HttpServletRequest参数

--- src/main/java/com/xinf/controller/NotifyController.java
+++ src/main/java/com/xinf/controller/NotifyController.java
@@ -6,6 +6,7 @@ import com.xinf.service.RoomService;
 import com.xinf.util.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,68 +39,115 @@ public class NotifyController {
     @Autowired
     RedisUtil redisUtil;
 
+    /**
+     * app: live
+     * flashver: FMLE/3.0 (compatible; FMSc/1.0)
+     * swfurl: rtmp://ts.memorykk.cn:1935/live
+     * tcurl: rtmp://ts.memorykk.cn:1935/live
+     * pageurl:
+     * addr: 113.140.84.104
+     * clientid: 12
+     * call: publish
+     * name: 6
+     * type: live
+     * @param request
+     * @return
+     */
     @RequestMapping("/publish")
     public boolean publish(HttpServletRequest request){
-//        Room room = roomService.getById(id);
-//        if(room != null) {
-//            room.setRoomStatus(1);
-//            roomService.updateById(room);
-//            log.info("房间号: {}开始推流", id);
-//            return true;
-//        }else{
-//            return false;
-//        }
-        Enumeration<String> parameterNames = request.getParameterNames();
-        while(parameterNames.hasMoreElements()){
-            String paraName=(String)parameterNames.nextElement();
+        System.out.println("-----------publish----------");
+        Enumeration enu=request.getParameterNames();
+        while(enu.hasMoreElements()){
+            String paraName=(String)enu.nextElement();
             System.out.println(paraName+": "+request.getParameter(paraName));
         }
-        return true;
-    }
 
-    @RequestMapping("/publish_done")
-    public boolean publish_done(@RequestParam long id){
-        Room room = roomService.getById(id);
+        String roomId = request.getParameter("name");
+        Room room = roomService.getById(roomId);
         if(room != null) {
-            room.setRoomStatus(0);
+            room.setRoomStatus(1);
             roomService.updateById(room);
-            log.info("房间号: {} 关闭推流", id);
+            log.info("房间号: {}开始推流", roomId);
             return true;
         }else{
             return false;
         }
     }
 
-    // roomName : 房间id
+    @RequestMapping("/publish_done")
+    public boolean publish_done(HttpServletRequest request){
+        System.out.println("-----------publish_done----------");
+        Enumeration enu=request.getParameterNames();
+        while(enu.hasMoreElements()){
+            String paraName=(String)enu.nextElement();
+            System.out.println(paraName+": "+request.getParameter(paraName));
+        }
+
+        String roomId = request.getParameter("name");
+        Room room = roomService.getById(roomId);
+        if(room != null) {
+            room.setRoomStatus(0);
+            roomService.updateById(room);
+            log.info("房间号: {} 关闭推流", roomId);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // roomId : 房间id
     @RequestMapping("/play")
-    public boolean play(@RequestParam String roomName){
+    public boolean play(HttpServletRequest request){
+        System.out.println("-----------play----------");
+        Enumeration enu=request.getParameterNames();
+        while(enu.hasMoreElements()){
+            String paraName=(String)enu.nextElement();
+            System.out.println(paraName+": "+request.getParameter(paraName));
+        }
+
         //if(redisUtil.zScore("recommand", roomName) == null){
         //    redisUtil.zAdd("recommand", roomName,1);
         //}else{
         //    redisUtil.zIncrementScore("recommand", roomName,1);
         //}
-        redisUtil.zIncrementScore("recommand", roomName,1);
+        String roomId = request.getParameter("name");
+        redisUtil.zIncrementScore("recommand", roomId,1);
         return true;
     }
 
     @RequestMapping("/play_done")
-    public boolean play_done(@RequestParam String roomName){
-        if(redisUtil.zScore("recommand", roomName).isPresent()){
-            if (redisUtil.zIncrementScore("recommand", roomName, -1).intValue() < 1 ) {
-                redisUtil.zRemove("recommand", roomName);
+    public boolean play_done(HttpServletRequest request){
+        System.out.println("-----------play_done----------");
+        Enumeration enu=request.getParameterNames();
+        while(enu.hasMoreElements()){
+            String paraName=(String)enu.nextElement();
+            System.out.println(paraName+": "+request.getParameter(paraName));
+        }
+
+        String roomId = request.getParameter("name");
+        if(redisUtil.zScore("recommand", roomId).isPresent()){
+            if (redisUtil.zIncrementScore("recommand", roomId, -1).intValue() < 1 ) {
+                redisUtil.zRemove("recommand", roomId);
             }
         }
         return true;
     }
 
     @RequestMapping("/record_done")
-    public boolean record_done(@RequestParam long roomId,
-                               @RequestParam String path){
-        Playback playback = new Playback();
-        playback.setRoomId(roomId);
-        playback.setPlaybackPath(path);
-        playbackService.save(playback);
-        log.info("直播间: {} 保存回放， 地址为 {}", roomId, path);
+    public boolean record_done(HttpServletRequest request){
+
+        System.out.println("-----------play----------");
+        Enumeration enu=request.getParameterNames();
+        while(enu.hasMoreElements()){
+            String paraName=(String)enu.nextElement();
+            System.out.println(paraName+": "+request.getParameter(paraName));
+        }
+
+//        Playback playback = new Playback();
+//        playback.setRoomId(roomId);
+//        playback.setPlaybackPath(path);
+//        playbackService.save(playback);
+//        log.info("直播间: {} 保存回放， 地址为 {}", roomId, path);
         return true;
     }
 }
